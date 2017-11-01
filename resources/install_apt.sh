5e8e6a072fbf825019975bfd66de4270e8412d33
ticed35 <ticed35@hotmail.com>
2017-11-01 08:52:52 -0400

    Dependancy installation process

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -1,7 +1,4 @@
-﻿PROGRESS_FILE=/tmp/dependancy_sonybravia_in_progress
-if [ ! -z $1 ]; then
-	PROGRESS_FILE=$1
-fi
+PROGRESS_FILE=$1
 touch ${PROGRESS_FILE}
 echo 0 > ${PROGRESS_FILE}
 sudo apt-get -y install python3
