1629e153377d5922dfb27f26587098ed87f28bdd
ticed35 <ticed35@hotmail.com>
2017-11-18 15:54:31 -0500

    Update dependancy requirements

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -2,6 +2,7 @@ PROGRESS_FILE=$1
 touch ${PROGRESS_FILE}
 echo 0 > ${PROGRESS_FILE}
 sudo apt-get -y install python3
+sudo apt-get -y install python3-requests
 echo 100 > ${PROGRESS_FILE}
 echo "Everything is successfully installed!"
 rm ${PROGRESS_FILE}
